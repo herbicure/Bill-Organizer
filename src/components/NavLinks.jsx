0d13e60569ca2cf06338d84f62a74c6430988fab
katharine benefico <kbenefico@gmail.com>
2016-09-25 13:33:03 -0400

    add userId to route paths

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -9,21 +9,22 @@ class NavLinks extends Component {
     super();
   }
   render() {
+    const userId = firebase.auth().currentUser.uid;
     return (
       <div>
         <div id="nav-links">
-          <Link to="/january" className="monthLinks" id="jan">January </Link>
-          <Link to="/february" className="monthLinks" id="feb">February </Link>
-          <Link to="/march" className="monthLinks" id="mar">March </Link>
-          <Link to="/april" className="monthLinks" id="apr">April </Link>
-          <Link to="/may" className="monthLinks" id="may">May </Link>
-          <Link to="/june" className="monthLinks" id="jun">June </Link>
-          <Link to="/july" className="monthLinks" id="jul">July </Link>
-          <Link to="/august" className="monthLinks" id="aug">August </Link>
-          <Link to="/september" className="monthLinks" id="sept">September </Link>
-          <Link to="/october" className="monthLinks" id="oct">October </Link>
-          <Link to="/november" className="monthLinks" id="nov">November </Link>
-          <Link to="/december" className="monthLinks" id="dec">December </Link>
+          <Link to={`/${userId}/january`} className="monthLinks" id="jan">January </Link>
+          <Link to={`/${userId}/february`} className="monthLinks" id="feb">February </Link>
+          <Link to={`/${userId}/march`} className="monthLinks" id="mar">March </Link>
+          <Link to={`/${userId}/april`} className="monthLinks" id="apr">April </Link>
+          <Link to={`/${userId}/may`} className="monthLinks" id="may">May </Link>
+          <Link to={`/${userId}/june`} className="monthLinks" id="jun">June </Link>
+          <Link to={`/${userId}/july`} className="monthLinks" id="jul">July </Link>
+          <Link to={`/${userId}/august`} className="monthLinks" id="aug">August </Link>
+          <Link to={`/${userId}/september`} className="monthLinks" id="sept">September </Link>
+          <Link to={`/${userId}/october`} className="monthLinks" id="oct">October </Link>
+          <Link to={`/${userId}/november`} className="monthLinks" id="nov">November </Link>
+          <Link to={`/${userId}/december`} className="monthLinks" id="dec">December </Link>
         </div>
       </div>
     );
