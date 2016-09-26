5c510292bf904f471a960d886eb35faefed4ff10
katharine benefico <kbenefico@gmail.com>
2016-09-26 16:15:27 -0400

    modify image link for launch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div id="home-screen">
       <h1 id="home-header"> A one stop shop for managing your expenses online! </h1>
-      <img id="wallet" src="./images/wallet.png" />
+      <img id="wallet" src="/images/wallet.png" />
         <div id="app-summary">
          <p>Simply log-in or register for immediate assistance with: <br /></p>
           <ul id="selling-points">
