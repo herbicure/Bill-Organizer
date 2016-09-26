d009f3cc8ceb519e62e57549822846e513000b87
katharine benefico <kbenefico@gmail.com>
2016-09-26 15:18:34 -0400

    create some html on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1> HOME SCREEN </h1>
-
+    <div id="home-screen">
+      <h1 id="home-header"> A one stop shop for managing your expenses online! </h1>
+      <img id="wallet" src="./images/wallet.png" />
+        <div id="app-summary">
+         <p>Monthly Bill Organizer offers help with: <br /></p>
+          <ul id="selling-points">
+            <li>Budget assistance</li>
+            <li>Spending trends</li>
+            <li>Payment punctuality</li>
+          </ul>
+        </div>
     </div>
   );
-}
+};
 
 export default Home;
