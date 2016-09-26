9c16a086bd91236f016e4e13d3c8614dd1b7e21f
katharine benefico <kbenefico@gmail.com>
2016-09-26 16:06:26 -0400

    prepare for launch

---
+++ src/components/Snapshot.jsx
@@ -0,0 +1,31 @@
+// import React, { Component } from 'react';
+// import Dashboard from './Dashboard.jsx';
+// import January from './months/January.jsx';
+
+// const propTypes = {
+//   janTotal: React.PropTypes.number,
+//   renderTest: React.PropTypes.func,
+// };
+
+// class Snapshot extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   componentDidMount() {
+//     console.log(this.props.janTotal);
+//   }
+//   render() {
+//     return (
+//       <div>
+//        {this.props.janTotal}
+//       </div>
+//     );
+//   }
+// }
+
+// Snapshot.propTypes = propTypes;
+
+// export default Snapshot;
+
+//
+// {this.props.renderTest ? janTotal=this.props.janTotal : false }
