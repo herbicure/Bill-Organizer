24b7311710a677c713f022d67a6974cc30400909
katharine benefico <kbenefico@gmail.com>
2016-09-26 15:16:48 -0400

    add more month components including dueDate

---
+++ src/components/months/August.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import firebase from '../../../firebase.config.js';
+import request from 'superagent';
+import BillPost from '../BillPost.jsx';
+import BillList from '../BillList.jsx';
+
+
+class August extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      billPosts: [],
+      augTotal: 0,
+    };
+    this.handlePosting = this.handlePosting.bind(this);
+    this.httpRemovePost = this.httpRemovePost.bind(this);
+    this.httpUpdatePost = this.httpUpdatePost.bind(this);
+    this.httpPublishPost = this.httpPublishPost.bind(this);
+  }
+  componentDidMount() {
+    this.httpGetPosts();
+  }
+  httpGetPosts() {
+    let userId = firebase.auth().currentUser.uid;
+    const url = `https://monthly-bill-organizer.firebaseio.com/users/${userId}/2016/august.json`;
+    request.get(url).then((response) => {
+      const postsData = response.body;
+      let billPosts = [];
+      if (postsData) {
+        billPosts = Object.keys(postsData).map((id) => {
+          const oneBill = postsData[id];
+          return {
+            id,
+            amount: oneBill.amount,
+            description: oneBill.description,
+            dueDate: oneBill.dueDate,
+          };
+        });
+        this.setState({ billPosts });
+        this.calculateTotal();
+      }
+    });
+  }
+  handlePosting({ id, amount, description, dueDate }) {
+    if (id) {
+      this.httpUpdatePost({ id, amount, description, dueDate });
+    } else {
+      this.httpPublishPost({ amount, description, dueDate });
+    }
+  }
+  httpRemovePost(id) {
+    let userId = firebase.auth().currentUser.uid;
+    const url = `https://monthly-bill-organizer.firebaseio.com/users/${userId}/2016/august/${id}.json`;
+    request.del(url).then(() => {
+      this.httpGetPosts();
+    });
+  }
+  httpUpdatePost({ id, amount, description, dueDate }) {
+    let userId = firebase.auth().currentUser.uid;
+    const url = `https://monthly-bill-organizer.firebaseio.com/users/${userId}/2016/august/${id}.json`;
+    request.patch(url).send({ id, amount, description, dueDate }).then(() => {
+      this.httpGetPosts();
+    });
+  }
+  httpPublishPost({ amount, description, dueDate }) {
+    let userId = firebase.auth().currentUser.uid;
+    const url = `https://monthly-bill-organizer.firebaseio.com/users/${userId}/2016/august.json`;
+    request.post(url).send({ amount, description }).then(() => {
+      this.httpGetPosts();
+    });
+  }
+  calculateTotal() {
+    let updatedTotal = 0;
+    this.state.billPosts.forEach((bill) => {
+      updatedTotal += parseFloat(bill.amount);
+      this.setState({
+        augTotal: updatedTotal.toFixed(2),
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <br />
+        <div id="august" className="month">
+          <h1 className="month-header">AUGUST 2016</h1>
+          <BillPost
+            handleRemove={this.httpRemovePost}
+            handlePosting={this.handlePosting}
+          />
+          <BillList
+            handleRemove={this.httpRemovePost}
+            handlePosting={this.handlePosting}
+            billPosts={this.state.billPosts}
+          />
+          <br />
+          <h2>TOTAL: <span id="tot"> ${this.state.augTotal}</span></h2>
+          <br />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default August;
