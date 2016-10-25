603ed0ffd22a06750a150f61dae89bef431c51ff
katharine benefico <kbenefico@gmail.com>
2016-10-25 16:46:49 -0400

    update home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 const Home = () => {
   return (
     <div id="home-screen">
-      <h1 id="home-header"> Manage your expenses online! </h1>
+      <h1 id="home-header"> Manage your expenses online!</h1>
       <img id="wallet" src="images/wallet.png" />
         <div id="app-summary">
           <p>Log-in or register for immediate assistance with: <br /></p>
